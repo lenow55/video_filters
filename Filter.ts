545cd9acb0b591948199fb585b2ef9ad8d79a632
Mishany52 <kalintsev309206@gmail.com>
2022-04-03 21:06:01 +0300

    Filter and VideoFilter

--- Filter.ts
+++ Filter.ts
@@ -1,5 +1,37 @@
-class Filter
+import { IRule } from "./Rules/IRule";
+import { RuleDate } from "./Rules/RuleDate";
+import { RuleName } from "./Rules/RuleName";
+
+export class Filter
 {
-    private linksRule: Rule;
-    private builder: RuleBuilder;
+    private linksRule: IRule[];
+    // private builder: RuleBuilder;
+
+    public setRule(rule: IRule): void{
+        this.linksRule.push(rule);
+    }
+
+    checkRules(content: any): boolean{
+        if (this.linksRule.length === 0){
+            return false;
+        }
+        else if (this.linksRule.filter(content)){
+            return true;
+        }
+    }
+
+
 }
+
+const ruleN = new RuleName();
+ruleN.setTime('First video')
+
+let filt = new Filter();
+let ruleD = new RuleDate();
+let date = new Date();
+ruleD.setDate(date);
+
+filt.setRule(ruleD);
+filt.setRule(ruleN);
+
+console.log(filt.checkRules(ruleD))

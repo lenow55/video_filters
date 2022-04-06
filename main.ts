e459ae2fca3d8be1ec3200498c99cee9785f281c
Новиков Илья <nov.ilucha@yandex.ru>
2022-04-06 22:48:18 +0300

    тестовая проверка правил

--- main.ts
+++ main.ts
@@ -4,3 +4,4 @@ import { VideoFilter } from "./Filters/VideoFilter"
 const builder: VideoFilterBuilder = new VideoFilterBuilder()
 
 let filter: VideoFilter = builder.build("bla bla bla")
+filter.getFilterString()

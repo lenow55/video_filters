5d4c9384f78e2305a816e25a02576fd76fd7a5eb
Новиков Илья <nov.ilucha@yandex.ru>
2022-04-06 22:48:00 +0300

    реализовал вывод всех правил в консоль

--- Filters/VideoFilter.ts
+++ Filters/VideoFilter.ts
@@ -1,5 +1,10 @@
 import { Filter } from "./Filter";
+import { IRule } from "../Rules/IRule";
 
 export class VideoFilter extends Filter {
-
+    getFilterString() {
+        this.linksRule.forEach( (rule: IRule) => {
+            console.log(rule)
+        });
+    }
 }
